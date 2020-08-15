8c9141c3ae3963e0bcdc7be6f34cfe3385996cd8
Kader Miyanyedi <Kadermiyanyedi@hotmail.com>
2020-08-15 12:18:07 +0300

    added admin.py

--- blog/api/admin.py
+++ blog/api/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Blog, Comment
 
-# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("title", "id", "author", "date")
+    list_filter = ("title", "id", "date")
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("post", "comment_author", "date")
+    list_filter = ("post", "date")
+
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Comment, CommentAdmin)
+
+admin.site.site_title = "Privia Staj CRUD Blog Api"
