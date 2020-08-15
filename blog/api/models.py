0e36881ff2c7a2767a43522e03ded69445d3a841
Kader Miyanyedi <Kadermiyanyedi@hotmail.com>
2020-08-15 12:17:48 +0300

    added models.py

--- blog/api/models.py
+++ blog/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    id = models.IntegerField(primary_key=True)
+    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    detail = models.TextField()
+    image = models.ImageField(blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    comment_text = models.TextField()
+    comment_author = models.CharField(max_length=50)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment for the {self.post}"
+
