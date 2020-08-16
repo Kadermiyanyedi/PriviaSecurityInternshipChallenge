ac8d05e3bdaee16226efb3a68d5f201b59051d36
Kader Miyanyedi <Kadermiyanyedi@hotmail.com>
2020-08-17 00:30:32 +0300

    added login url and search url

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,11 +1,26 @@
 from django.urls import path
-from .views import BlogAdd, BlogDeleteView, BlogDetailView, BlogListView, BlogUpdate
+from .views import (
+    BlogAdd,
+    BlogDeleteView,
+    BlogDetailView,
+    BlogListView,
+    BlogUpdate,
+    LoginUserView,
+    SearchView,
+)
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
-    path("", BlogListView.as_view(), name="index"),
+    path("", LoginUserView.as_view(), name="login"),
+    path("index/", BlogListView.as_view(), name="index"),
     path("add/", BlogAdd.as_view(), name="add"),
-    path("detail/<int:pk>", BlogDetailView.as_view(), name="detail"),
+    path("detail/<int:pk>", BlogDetailView, name="detail"),
     path("delete/<int:pk>", BlogDeleteView.as_view(), name="delete"),
     path("update/<int:pk>", BlogUpdate.as_view(), name="update"),
+    path("search/", SearchView.as_view(), name="search"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
